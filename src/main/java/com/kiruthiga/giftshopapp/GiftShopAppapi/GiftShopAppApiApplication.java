1677167cf92cc1aa2365d94ae0a5824ba534a184
kiruthiga2408 <keerthilalli24@gmail.com>
2022-04-27 10:05:25 +0530

    added features

---
+++ src/main/java/com/kiruthiga/giftshopapp/GiftShopAppapi/GiftShopAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.kiruthiga.giftshopapp.GiftShopAppapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftShopAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftShopAppApiApplication.class, args);
+	}
+
+}
